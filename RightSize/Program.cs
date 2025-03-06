aa5e44bf29b051ae4911bd61451acecd8d66db71
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-03-06 16:02:06 +0000

    Exercicio 1

---
+++ RightSize/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+            foreach(string s in args)
+            {
+
+                 if(s.Length>=8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                else if(s.Length>3)
+                {
+                    Console.WriteLine(s);
+                }
+               
+            }
+        }
+    }
+}
