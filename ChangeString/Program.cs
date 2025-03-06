f1ebcd3b17f6dbbc286a9e125427d37cbfded1a8
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-03-06 16:13:39 +0000

    Exercicio 2

---
+++ ChangeString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("String:");
+            string str =Console.ReadLine();
+            Console.WriteLine("Caráter:");
+            string ctr=Console.ReadLine();
+            char carater=char.Parse(ctr);
+
+            foreach(char l in str)
+            {
+                if(carater==l)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(l);
+                }
+            }
+
+        }
+    }
+}
